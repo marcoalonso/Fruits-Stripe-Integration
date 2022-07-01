b2572d0bbc4e71b94f952cb915b0601d66ad4599
Marco Alonso <marco@garagecoders.net>
2022-07-01 15:00:24 -0500

    Empezando

---
+++ Fruits-Stripe-Integration/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fruits-Stripe-Integration
+//
+//  Created by marco rodriguez on 01/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fruits-Stripe-Integration/Fruits_Stripe_IntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fruits_Stripe_IntegrationApp.swift
+//  Fruits-Stripe-Integration
+//
+//  Created by marco rodriguez on 01/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Fruits_Stripe_IntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
